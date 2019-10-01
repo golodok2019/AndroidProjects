156b180a4a975956d544c7500b4d0cf427f3f4ec
golodok2019 <aleksei-golod@mail.ru>
2019-10-01 12:38:13 +0300

    0.1.1

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -16,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textAndroid);
         textView.setText(String.format(Locale.getDefault(), "Android: %s (%d)", Build.VERSION.RELEASE, Build.VERSION.SDK_INT));
 
+        TextView versionName = findViewById(R.id.textName);
+        versionName.setText(String.format(Locale.getDefault(),"Name: %s",BuildConfig.VERSION_NAME));
+
+        TextView versionCode = findViewById(R.id.textCode);
+        versionCode.setText(String.format(Locale.getDefault(),"Code: %d",BuildConfig.VERSION_CODE));
     }
 }
