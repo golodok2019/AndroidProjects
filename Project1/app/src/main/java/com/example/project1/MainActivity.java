343ead427f832a9d40082f7da99275608e22ee78
golodok2019 <aleksei-golod@mail.ru>
2019-10-15 10:54:19 +0300

    0.2.2

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -1,27 +1,91 @@
 package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private CustomDialogFragment dialog;
+
+    private static final int REQUEST_CODE_READ_PHONE = 1;
+    private static boolean READ_PHONE_GRANTED = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TextView view = (TextView) findViewById (R.id.textAndroid);
+        //dialog = new CustomDialogFragment();
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textAndroid);
-        textView.setText(String.format(Locale.getDefault(), "Android: %s (%d)", Build.VERSION.RELEASE, Build.VERSION.SDK_INT));
-
         TextView versionName = findViewById(R.id.textName);
-        versionName.setText(String.format(Locale.getDefault(),"Name: %s",BuildConfig.VERSION_NAME));
+        versionName.setText(String.format(Locale.getDefault(), "Name: %s", BuildConfig.VERSION_NAME));
 
         TextView versionCode = findViewById(R.id.textCode);
-        versionCode.setText(String.format(Locale.getDefault(),"Code: %d",BuildConfig.VERSION_CODE));
+        versionCode.setText(String.format(Locale.getDefault(), "Model: %s", android.os.Build.MODEL));
+
+        TextView view = (TextView) findViewById(R.id.textAndroid);
+
+        int hasReadContactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        // если устройство до API 23, устанавливаем разрешение
+        if (hasReadContactPermission == PackageManager.PERMISSION_GRANTED) {
+            READ_PHONE_GRANTED = true;
+        } else {
+             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Alert!")
+                  .setMessage("Requer!")
+                  .setCancelable(false)
+                  .setNegativeButton("OK",
+                    new DialogInterface.OnClickListener() {
+                      public void onClick(DialogInterface dialog, int id) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+                      dialog.cancel();
+                }
+            });
+             builder.show();
+            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE_READ_PHONE);
+        }
+        if (READ_PHONE_GRANTED) {
+            TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+            view.setText(TelephonyMgr.getDeviceId());
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        TextView view = (TextView) findViewById(R.id.textAndroid);
+        switch (requestCode) {
+            case REQUEST_CODE_READ_PHONE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    READ_PHONE_GRANTED = true;
+                }
+        }
+        if (READ_PHONE_GRANTED) {
+            TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED || READ_PHONE_GRANTED == true) {
+                view.setText(TelephonyMgr.getDeviceId());
+            }
+        }
+        else{
+            view.setText("Need set permition");
+        }
     }
 }
+
